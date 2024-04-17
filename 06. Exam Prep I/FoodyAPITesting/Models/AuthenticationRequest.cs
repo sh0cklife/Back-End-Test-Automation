b49823009df8848915daaae36cfaa089c5b545b6
Denis Atanassov <dennis.atanasoff@gmail.com>
2024-04-17 20:21:23 +0300

    feat added: exam prep

---
+++ 06. Exam Prep I/FoodyAPITesting/Models/AuthenticationRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FoodyAPITesting.Models
+{
+    public class AuthenticationRequest
+    {
+        [JsonPropertyName("username")]
+        public string UserName { get; set;}
+
+        [JsonPropertyName("password")]
+        public string Password { get; set;}
+        
+    }
+}
---
+++ 06. Exam Prep I/FoodyAPITesting/Models/AuthenticationResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FoodyAPITesting.Models
+{
+    public class AuthenticationResponse
+    {
+        [JsonPropertyName("username")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("accessToken")]
+        public string AccessToken { get; set; }
+    }
+}
